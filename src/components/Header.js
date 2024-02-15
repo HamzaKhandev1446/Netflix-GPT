fdd29a91279833bd1aa16ef75499970c392f235b
Hamza Khan <hamza.khan@speridian.com>
2024-02-15 14:34:58 +0500

    Header component structure created.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,19 @@
 import React from "react";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="w-full z-10 flex flex-row justify-start align-middle bg-gradient-to-b from-black">
+      <div className="w-1/6 flex justify-center items-center">
+        <img
+          src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
+          alt="Netflix Logo"
+        />
+      </div>
+      <div className="flex flex-col justify-center align-middle ml-auto">
+        <p className="text-white text-bold"> Right Header Section</p>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,17 @@
 import React from "react";
+import Header from "./Header";
 
 const Login = () => {
-  return <div>Login</div>;
+  const imageUrl =
+    "https://miro.medium.com/v2/resize:fit:1400/1*5lyavS59mazOFnb55Z6znQ.png";
+  return (
+    <div
+      className="bg-cover bg-top relative h-screen"
+      style={{ backgroundImage: `url(${imageUrl})` }}
+    >
+      <Header />
+    </div>
+  );
 };
 
 export default Login;
